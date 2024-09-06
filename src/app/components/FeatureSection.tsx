57b0e2a0bc9e57e437fd6a606987ed6585635ed4
Prashant <prashant.pyv@gmail.com>
2024-09-06 16:55:39 +0530

    SVGs added in feature list view

--- src/app/components/FeatureSection.tsx
+++ src/app/components/FeatureSection.tsx
@@ -55,11 +55,18 @@ export default function FeaturesSection({ claimsData }: FeatureSectionProps) {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-wrap justify-around gap-8 mb-8 ">
           {claimsrow1.map((claim) => (
-            <div key={claim.id} className="bg-gray-800 text-white p-4 rounded-full h-[350px] w-[350px] flex flex-col justify-around items-center">
-              <h1 className="text-[18px] font-[600] leading-[20px] font-[Favorit, sans-serif] mb-2 mx-3 text-center">
+            <div key={claim.id} className="bg-gray-600 flex flex-col text-white p-8 pt-12 h-[350px] w-[350px]  items-center relative">
+              <Image
+                className=" absolute top-[-20px] left-[-20px]"
+                src={`/svgs/icon${claim.id}.drawio.svg`}
+                alt="Logo"
+                width={80}
+                height={80}
+                />
+              <h1 className="text-[20px] font-[600] leading-[30px] font-[Favorit, sans-serif] mb-12 inline">
                 {claim.attributes.ChannelVariants.Website}
               </h1>
-              {/* <p className="text-[12px] font-[400] leading-[16px] text-[#001439] font-[Favorit, sans-serif] text-center">
+              {/* <p className="text-[18px] font-[400] leading-[20px] text-gray-200 font-[Favorit, sans-serif]">
                 {claim.attributes.ChannelVariants.Website}
               </p> */}
             </div>
@@ -67,23 +74,37 @@ export default function FeaturesSection({ claimsData }: FeatureSectionProps) {
         </div>
         <div className="flex flex-wrap justify-around gap-8 mb-8">
           {claimsrow2.map((claim) => (
-            <div key={claim.id} className="bg-gray-200 p-4 rounded-full h-[350px] w-[350px] flex flex-col justify-around items-center">
-              <h1 className="text-[18px] font-[600] leading-[20px] text-gray-800 font-[Favorit, sans-serif] mb-2 mx-3 text-center">
-                {claim.attributes.ChannelVariants.Website}
-              </h1>
-              {/* <p className="text-[12px] font-[400] leading-[16px] text-[#001439] font-[Favorit, sans-serif] text-center">
-                {claim.attributes.ChannelVariants.Website}
-              </p> */}
-            </div>
+            <div key={claim.id} className="bg-gray-200 flex flex-col text-gray-800 p-8 pt-12 h-[350px] w-[350px]  items-center relative">
+            <Image
+              className=" absolute top-[-20px] left-[-20px]"
+              src={`/svgs/icon${claim.id}.drawio.svg`}
+              alt="Logo"
+              width={80}
+              height={80}
+              />
+            <h1 className="text-[20px] font-[600] leading-[30px] font-[Favorit, sans-serif] mb-12 inline">
+              {claim.attributes.ChannelVariants.Website}
+            </h1>
+            {/* <p className="text-[18px] font-[400] leading-[20px] text-gray-200 font-[Favorit, sans-serif]">
+              {claim.attributes.ChannelVariants.Website}
+            </p> */}
+          </div>
           ))}
         </div>
         <div className="flex flex-wrap justify-around gap-8 mb-8 ">
           {claimsrow3.map((claim) => (
-            <div key={claim.id} className="bg-gray-800 text-white p-4 rounded-full h-[350px] w-[350px] flex flex-col justify-around items-center">
-              <h1 className="text-[18px] font-[600] leading-[20px] font-[Favorit, sans-serif] mb-2 mx-3 text-center">
+              <div key={claim.id} className="bg-gray-600 flex flex-col text-white p-8 pt-12 h-[350px] w-[350px]  items-center relative">
+              <Image
+                className=" absolute top-[-20px] left-[-20px]"
+                src={`/svgs/icon${claim.id}.drawio.svg`}
+                alt="Logo"
+                width={80}
+                height={80}
+                />
+              <h1 className="text-[24px] font-[600] leading-[30px] font-[Favorit, sans-serif] mb-12 inline">
                 {claim.attributes.ChannelVariants.Website}
               </h1>
-              {/* <p className="text-[12px] font-[400] leading-[16px] text-[#001439] font-[Favorit, sans-serif] text-center">
+              {/* <p className="text-[18px] font-[400] leading-[20px] text-gray-200 font-[Favorit, sans-serif]">
                 {claim.attributes.ChannelVariants.Website}
               </p> */}
             </div>
@@ -91,14 +112,21 @@ export default function FeaturesSection({ claimsData }: FeatureSectionProps) {
         </div>
         <div className="flex flex-wrap justify-around gap-8 mb-8">
           {claimsrow4.map((claim) => (
-            <div key={claim.id} className="bg-gray-200 p-4 rounded-full h-[350px] w-[350px] flex flex-col justify-around items-center">
-              <h1 className="text-[18px] font-[600] leading-[20px] text-gray-800 font-[Favorit, sans-serif] mb-2 mx-3 text-center">
-                {claim.attributes.ChannelVariants.Website}
-              </h1>
-              {/* <p className="text-[12px] font-[400] leading-[16px] text-[#001439] font-[Favorit, sans-serif] text-center">
-                {claim.attributes.ChannelVariants.Website}
-              </p> */}
-            </div>
+                        <div key={claim.id} className="bg-gray-200 flex flex-col text-gray-800 p-8 pt-12 h-[350px] w-[350px]  items-center relative">
+                        <Image
+                          className=" absolute top-[-20px] left-[-20px]"
+                          src={`/svgs/icon${claim.id}.drawio.svg`}
+                          alt="Logo"
+                          width={80}
+                          height={80}
+                          />
+                        <h1 className="text-[20px] font-[600] leading-[30px] font-[Favorit, sans-serif] mb-12 inline">
+                          {claim.attributes.ChannelVariants.Website}
+                        </h1>
+                        {/* <p className="text-[18px] font-[400] leading-[20px] text-gray-200 font-[Favorit, sans-serif]">
+                          {claim.attributes.ChannelVariants.Website}
+                        </p> */}
+                      </div>
           ))}
         </div>
       </div>
