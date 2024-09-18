8fa9e45e147734e44c4893b20d8096c0ea2b3460
Prashant <prashant.pyv@gmail.com>
2024-09-18 17:08:13 +0530

    Elevate Experience section added

--- src/app/components/ElevateExperienceSection.jsx
+++ src/app/components/ElevateExperienceSection.jsx
@@ -5,7 +5,7 @@ export default function ElevateExperienceSection() {
         <div className="max-w-6xl mx-auto px-4 sm:px-6 py-8">
             <div className="flex flex-col lg:flex-row-reverse items-center justify-between gap-1 p-6">
                 <div className="lg:w-1/2">
-                    <h1 className="text-[#0055c8] text-3xl font-bold mb-4">
+                    <h1 className="text-[#0055c8] text-4xl font-bold mb-4">
                         Elevate Your Search Experience
                     </h1>
                     <div className="flex flex-col gap-4">
@@ -17,7 +17,7 @@ export default function ElevateExperienceSection() {
                         </p>
                     </div>
                 </div>
-                <div className="relative w-full h-[400px] lg:h-[300px]">
+                <div className="relative w-1/2 h-[400px] lg:h-[300px]">
                     <Image
                         src={"/svgs/triangles.svg"}
                         alt="triangle"
