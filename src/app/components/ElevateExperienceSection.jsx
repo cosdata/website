f9089d0c5ff84a57ca43623bd5dba3924b61e5a5
Prashant <prashant.pyv@gmail.com>
2024-09-19 16:35:51 +0530

    Elevate Experience Design edits

--- src/app/components/ElevateExperienceSection.jsx
+++ src/app/components/ElevateExperienceSection.jsx
@@ -9,15 +9,15 @@ export default function ElevateExperienceSection() {
             <div className="flex flex-col lg:flex-row-reverse items-center justify-between gap-4 lg:gap-1 lg:p-6">
                 <div className="lg:w-1/2">
 
-                    <div className="flex flex-col  gap-8">
+                    <div className="flex flex-col  gap-12">
                         <p className="text-[#3d8bff] leading-relaxed flex flex-col">
-                            <span className="text-[#f23665] text-3xl font-semibold">Hybrid Search</span>
+                            <span className="text-[#f23665] text-3xl font-semibold mb-3">Hybrid Search</span>
                             <span className='leading-[30px]'>
                                 Enhance search precision with our vector database, leveraging the power of combined sparse and dense vector searches to deliver highly relevant, context-rich results for complex queries.
                             </span>
                         </p>
                         <p className="text-[#3d8bff] leading-relaxed flex flex-col">
-                            <span className="text-[#f23665] text-3xl font-semibold">Knowledge-Graph Integration</span>
+                            <span className="text-[#f23665] text-3xl font-semibold mb-3">Knowledge-Graph Integration</span>
                             <span className='leading-[30px]'>
                                 Enhance relevance by mapping structured data to vector embeddings. Seamlessly blend ontological relationships with contextual semantics.
                             </span>
