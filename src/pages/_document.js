0e6c6b83b7e3a4fd3c57faa9bed4da780a87ec69
jackson-laskey <jackson.laskey@gmail.com>
2024-10-08 13:45:06 -0400

    Add google analytics

---
+++ src/pages/_document.js
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QK60MJRHMR"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QK60MJRHMR');
+          `
+        }} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
